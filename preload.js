050dff90c0b876349a1d21a78ffff012fd7f6fc5
karthikjpt <30523981+karthikjpt@users.noreply.github.com>
2025-05-22 02:06:39 +0530

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,8 +1,9 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-// Expose limited, safe API to renderer process
 contextBridge.exposeInMainWorld('electronAPI', {
   sendMessage: (msg) => ipcRenderer.send('message', msg),
-  onMessage: (callback) => ipcRenderer.on('reply', (event, data) => callback(data))
-});
+  onMessage: (callback) => ipcRenderer.on('reply', (event, data) => callback(data)),
 
+  // Add closeWindow method
+  closeWindow: () => ipcRenderer.send('close-window')
+});
