1d66df5dcf4ae96e73d8ec260ba70e009f2b4ca8
karthik <30523981+karthikjpt@users.noreply.github.com>
2025-06-09 20:37:38 +0530

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,6 +1,25 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  closeWindow: () => ipcRenderer.send('close-window')
-});
+  // Send request to close the window
+  closeWindow: () => ipcRenderer.send('close-window'),
 
+  // Send request to get the saved startup preference
+  getStartupPreference: () => {
+    ipcRenderer.send('get-startup-preference');
+  },
+
+  // Listen for startup preference value from main process
+  onStartupPreference: (callback) => {
+    if (typeof callback === 'function') {
+      ipcRenderer.on('startup-preference', (event, enabled) => {
+        callback(Boolean(enabled));
+      });
+    }
+  },
+
+  // Set startup preference (true or false)
+  setStartupPreference: (enabled) => {
+    ipcRenderer.send('set-startup-preference', Boolean(enabled));
+  }
+});
