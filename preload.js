553e3096f7ee450ebd0a000880ea7885b1bb5441
karthik <30523981+karthikjpt@users.noreply.github.com>
2026-02-18 19:36:31 +0530

    Expose OGDP API in preload.js

--- preload.js
+++ preload.js
@@ -1,5 +1,16 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-contextBridge.exposeInMainWorld('electronAPI', {
-  closeWindow: () => ipcRenderer.send('close-window')
+contextBridge.exposeInMainWorld('OGDP', {
+  window: {
+    close: () => ipcRenderer.send('close-window')
+  },
+  data: {
+    // Example: request data from main process (returns a Promise)
+    fetch: () => ipcRenderer.invoke('get-data')
+  },
+  settings: {
+    // Example: update or get settings via IPC
+    update: (key, value) => ipcRenderer.send('update-settings', { key, value }),
+    get: (key) => ipcRenderer.invoke('get-settings', key)
+  }
 });
